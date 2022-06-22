1fb2d6da7659186227d705fbc40c204017818a30
DennisJav <denis.javier.tapia.ortiz@gmail.com>
2022-06-21 19:50:55 -0500

    Taller 13

---
+++ src/main/java/com/uce/ec/ProyectoU2DtApplication.java
@@ -0,0 +1,13 @@
+package com.uce.ec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoU2DtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoU2DtApplication.class, args);
+	}
+
+}
