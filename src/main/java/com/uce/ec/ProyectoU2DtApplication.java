ec379611d0842083570b0e1af20f81b2f281aeb5
DennisJav <denis.javier.tapia.ortiz@gmail.com>
2022-06-27 12:19:27 -0500

    TAREA 12 LOG

--- src/main/java/com/uce/ec/ProyectoU2DtApplication.java
+++ src/main/java/com/uce/ec/ProyectoU2DtApplication.java
@@ -1,5 +1,7 @@
 package com.uce.ec;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProyectoU2DtApplication implements CommandLineRunner{
 
+	private static final Logger LOG = LogManager.getLogger( ProyectoU2DtApplication.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoU2DtApplication.class, args);
 	}
@@ -15,6 +19,7 @@ public class ProyectoU2DtApplication implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
 		System.out.println("Hola");
+		LOG.info("Hola con log");
 	}
 
 }
